4517cdccdbe2b6cf1179ddd4f5a3bf0fd4f75948
johnpospisil <audiofreak7@gmail.com>
2021-07-22 17:06:06 -0700

    Scale and rotate the ship

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ pygame.display.set_caption("Pygame Intro!")
 WHITE = (255, 255, 255)
 
 FPS = 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load('.//Assets/spaceship_yellow.png')
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 RED_SPACESHIP_IMAGE = pygame.image.load('.//Assets/spaceship_red.png')
+RED_SPACESHIP = pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
 
 
 def draw_window():
     WIN.fill(WHITE)
-    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+    WIN.blit(YELLOW_SPACESHIP, (300, 100))
     pygame.display.update()
 
 
